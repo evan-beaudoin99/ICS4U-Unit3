e92b883817865c0f43f6748d9e67c45e16958502
Evan Beaudoin <evan.beaudoin@mths.ca>
2024-05-22 11:00:48 -0400

    Init

--- 3-02/Java/Factorial.java
+++ 3-02/Java/Factorial.java
@@ -9,60 +9,61 @@
 import java.util.Scanner;
 
 /**
-* This program calculates a factorial.
-*/
-
+ * This program calculates a factorial.
+ */
 final class Factorial {
     /**
-    * Prevent instantiation
-    * Throw an exception IllegalStateException.
-    * if this ever is called
-    *
-    * @throws IllegalStateExceptioni if called
-    *
-    */
+     * Prevent instantiation
+     * Throw an exception IllegalStateException.
+     * if this ever is called
+     *
+     * @throws IllegalStateException if called
+     */
     private Factorial() {
         throw new IllegalStateException("Cannot be instantiated");
     }
 
     /**
-    * Calculates the factorial of a number.
-    *
-    * @param userNumber userNumber
-    * @return return factorialNumber
-    */
+     * Calculates the factorial of a number.
+     *
+     * @param userNumber userNumber
+     * @return return factorialNumber
+     */
     public static int factorialCalculation(final int userNumber) {
-        // process
-        final int returnValue;
-
-        if (userNumber == 0) {
-            returnValue = 1;
-        } else if (userNumber < 0) {
-            returnValue = -1;
+        // Handle edge cases
+        if (userNumber < 0) {
+            throw new IllegalArgumentException("Number must be non-negative.");
+        } else if (userNumber == 0) {
+            return 1;
         } else {
-            returnValue = userNumber * factorialCalculation(userNumber - 1);
+            // Calculate factorial using recursion
+            return userNumber * factorialCalculation(userNumber - 1);
         }
-        return returnValue;
     }
 
     /**
-    * The starting main() function.
-    *
-    * @param args No args will be used
-    */
+     * The starting main() function.
+     *
+     * @param args No args will be used
+     */
     public static void main(final String[] args) {
         try {
             // input & output
-            final Scanner userInput = new Scanner(System.in);
+            Scanner userInput = new Scanner(System.in);
             System.out.print("Enter a number: ");
-            final float roundedValue = userInput.nextFloat();
-            final int numberInput = Math.round(roundedValue);
-            System.out.print(factorialCalculation(numberInput));
-        }
-        catch (IllegalArgumentException ex) {
-            System.out.println("That is an invalid input.");
-        }
-        System.out.println("\nDone.");
+            int numberInput = userInput.nextInt();
+
+            // Calculate factorial
+            int result = factorialCalculation(numberInput);
+
+            // Print the result
+            System.out.println(result);
         
+        } catch (IllegalArgumentException ex) {
+            System.out.println("That is an invalid input.");
+        } finally {
+            System.out.println("\nDone.");
+        }
     }
 }
+
